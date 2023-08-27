ccdb2a32e1934c6f549e745aace07e7fcc5e716f
mecarte-ai <mecarte.ai@gmail.com>
2023-08-28 01:35:00 +0800

    add modal and display todolist items

--- src/App.js
+++ src/App.js
@@ -1,3 +1,39 @@
+import data from "./data";
+import "./App.css";
+import { useState } from "react";
+
 export default function App() {
-  return <div>Hello World!</div>;
+  const [showModal, setShowModal] = useState(false);
+  const [todolists, setTodolists] = useState(data);
+
+  function handleToggle() {
+    setShowModal(!showModal);
+  }
+
+  return (
+    <div>
+      <TodoList items={todolists} />
+      <button onClick={handleToggle}>Add todo</button>
+      {showModal && <Modal onToggle={handleToggle} />}
+    </div>
+  );
+}
+
+function Modal({ onToggle }) {
+  return (
+    <div className="modal">
+      <div className="backdrop" onClick={onToggle}></div>
+      <div className="modal-content">Hello World!</div>
+    </div>
+  );
+}
+
+function TodoList({ items }) {
+  return (
+    <ul>
+      {items.map((item) => (
+        <li>{item.content}</li>
+      ))}
+    </ul>
+  );
 }
---
+++ src/data.js
@@ -0,0 +1,19 @@
+const todos = [
+  {
+    id: 1,
+    content: "Learn React",
+    status: "Completed",
+  },
+  {
+    id: 2,
+    content: "Create React Projects",
+    status: "Not completed",
+  },
+  {
+    id: 3,
+    content: "Add Projects to Portfolio",
+    status: "Not completed",
+  },
+];
+
+export default todos;
