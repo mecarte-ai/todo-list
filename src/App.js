7dc8ace67071b4761730ae5e0790a8e55a64ce28
mecarte-ai <mecarte.ai@gmail.com>
2023-08-28 01:54:33 +0800

    add todo item adding functionality

--- src/App.js
+++ src/App.js
@@ -10,20 +10,27 @@ export default function App() {
     setShowModal(!showModal);
   }
 
+  function handleAddItem(item) {
+    setTodolists((items) => [...items, item]);
+    handleToggle();
+  }
+
   return (
     <div>
       <TodoList items={todolists} />
       <button onClick={handleToggle}>Add todo</button>
-      {showModal && <Modal onToggle={handleToggle} />}
+      {showModal && <Modal onToggle={handleToggle} onAddItem={handleAddItem} />}
     </div>
   );
 }
 
-function Modal({ onToggle }) {
+function Modal({ onToggle, onAddItem }) {
   return (
     <div className="modal">
       <div className="backdrop" onClick={onToggle}></div>
-      <div className="modal-content">Hello World!</div>
+      <div className="modal-content">
+        <ModalForm onAddItem={onAddItem} />
+      </div>
     </div>
   );
 }
@@ -37,5 +44,36 @@ function TodoList({ items }) {
         </li>
       ))}
     </ul>
+  );
+}
+
+function ModalForm({ onAddItem }) {
+  const [content, setContent] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!content) return;
+
+    const newItem = {
+      id: crypto.randomUUID,
+      content: content,
+      status: false,
+    };
+
+    onAddItem(newItem);
+    setContent("");
+  }
+
+  return (
+    <form className="modal-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Write content here"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
